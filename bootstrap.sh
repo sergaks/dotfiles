c23f792ad733e7eb688be8cdbcf055e08301c860
Sergei Aksenenko <serg.aksenenko@gmail.com>
2017-06-16 22:24:53 +0300

    Customized after review

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,15 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
-
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude ".idea/" \
+		--exclude "*.iml" \
+		--exclude "setup/" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
